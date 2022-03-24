f23d60b6e1b9316e4b19a72c3aec8eac689b8407
Jewel Carrier-Davis <jujicb@gmail.com>
2022-03-24 12:13:20 -0400

    commit tree 551711e6ad248d2166e47edb3bb515c808c38066

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName () {
+    // let upCaseCustomerName = String(customerName);
+    // // let finalCustomerName = upCaseCustomerName.toUpperCase();
+    // // return finalCustomerName
+    // upCaseCustomerName.toUpperCase()
+    // upCaseCustomerName = String(customerName);
+    customerName = customerName.toUpperCase();
+    // customerName
+    //customerName = customerName.toUpperCase();
+    //customerName = 'BOB';
+    return customerName
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'jimmy'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'agatha';
+    return leastFavoriteCustomer
+}
